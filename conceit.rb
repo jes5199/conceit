2483a3045bdf29881382d3c1018b3259b346d5a9
Jesse Wolfe <jes5199@gmail.com>
2011-03-11 13:42:11 -0800

    proof of a terrible concept

---
+++ conceit.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+require 'sinatra'
+require 'uri'
+
+#TODO POST data should cat to STDIN
+get /(.*)/ do
+  parts = params[:captures].first.split('/')
+  cmd_line = parts.find_all{|x| !x.empty?}.map{|x| URI.unescape(x).inspect }
+  command = cmd_line.shift
+
+  request.params.each do |key, value|
+    if key.length == 1
+      if value.nil?
+        cmd_line.unshift " -#{key}"
+      else
+        cmd_line.unshift " -#{key} #{value.inspect}"
+      end
+    else
+      if value.nil?
+        cmd_line.unshift " --#{key}"
+      else
+        cmd_line.unshift " --#{key}=#{value.inspect}"
+      end
+    end
+  end
+  cmd_line.unshift command
+  cmd_line = cmd_line.join(" ")
+
+  STDERR.puts cmd_line
+
+  output = `#{cmd_line} 2>&1`
+  status = $?.to_i
+  if status != 0
+    [ 500 + status, output ]
+  end
+  output
+end
